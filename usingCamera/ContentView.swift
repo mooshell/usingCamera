97997b7710b07e3d573692e22de00d4b57be29b8
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-05-28 00:44:43 -0400

    a fun short photo app in progress (week nine output)

--- usingCamera/ContentView.swift
+++ usingCamera/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isImagePickerShowing = false
+    @State var selectedImage: UIImage?
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 1.1, green: 0.96, blue: 0.96) // Baby pink background color
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Photograph Memories")
+                    .bold()
+                Text("Make or View Favorite Memories!")
+                Image(uiImage: selectedImage ?? UIImage(named: "camera")!)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 200)
+                    .clipped()
+                Text("Select an option below!")
+                    .font(.body)
+                    .foregroundColor(.pink)
+                    .padding()
+                HStack {
+                    Button {
+                        self.sourceType = .photoLibrary
+                        isImagePickerShowing = true
+                        // to do
+                    } label: {
+                        Text("Select a picture")
+                            .padding()
+                        .foregroundColor(.purple) }
+                    Button {
+                        self.sourceType = .camera
+                        isImagePickerShowing = true
+                        //to do
+                    } label: {
+                        Text("Take a picture")
+                            .padding()
+                        .foregroundColor(.purple) }
+                }
+            }
+            .sheet(isPresented: $isImagePickerShowing) {
+                        ImagePicker(selectedImage: $selectedImage, isImagePickerShowing: $isImagePickerShowing, sourceType: self.sourceType)
+                    }
+                
+
         }
-        .padding()
     }
 }
 
