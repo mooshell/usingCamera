0103ba9ef0e4b53aefb724f1ed7dffe9ab2135dd
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-05-27 23:32:47 -0400

    Initial Commit

---
+++ usingCamera/usingCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  usingCameraApp.swift
+//  usingCamera
+//
+//  Created by Michelle Elias Flores on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct usingCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
